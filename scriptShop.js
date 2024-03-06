e134f618a2f4e33d29cca25cc9639ad49c3799fa
MINGW64 <Adrijesuslerma@outlook.com>
2024-03-06 15:11:22 -0700

    proyecto comentado

--- scriptShop.js
+++ scriptShop.js
@@ -1,45 +1,41 @@
-const productsEl = document.getElementById('products');
-const titleFilterEl = document.getElementById('title-filter');
-const categoryFilterEl = document.getElementById('category-filter');
+const productsEl = document.getElementById("products");
+const titleFilterEl = document.getElementById("title-filter");
+const categoryFilterEl = document.getElementById("category-filter");
 
-async function getProducts (title, category){
+async function getProducts(title, category) {
+  let url = "https://fakestoreapi.com/products";
 
-    let url = 'https://fakestoreapi.com/products';
-
-    if (title || category){
-        url += '?';
-        if(title){
-            url += `title=${title}&`;
-        }
-
-        if(category){
-            url += `category=${category}`;
-        }
+  if (title || category) {
+    url += "?";
+    if (title) {
+      url += `title=${title}&`;
     }
 
+    if (category) {
+      url += `category=${category}`;
+    }
+  }
 
-    const response = await fetch(url);
-    const data = await response.json(); 
-    console.log(data);
-    return data;
-
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 // la funcion que va a renderizar los elementos dentro del DOM
 
-async function displayProducts (title, category) {
-    
-    // Obtener los productos filtrados
-    const products = await getProducts(title, category);
+async function displayProducts(title, category) {
+  // Obtener los productos filtrados
+  const productos = await getProducts(title, category);
 
-    productsEl.innerHTML = '';
+  productsEl.innerHTML = "";
 
-    // mostrar los productos
-    for( let product of products ){
-        const card = document.createElement('div');
-        card.classList.add('product-card');
+  // mostrar los productos
+  for (let product of productos) {
+    const card = document.createElement("div");
+    card.classList.add("product-card");
 
-        card.innerHTML = `
+    card.innerHTML = `
             <img  src="${product.image}" />
             <h5> ${product.title} </h5>
             <p> Categoria: ${product.category} </p>
@@ -51,23 +47,19 @@ async function displayProducts (title, category) {
 
         `;
 
-        productsEl.appendChild(card);
-
-    }
-
+    productsEl.appendChild(card);
+  }
 }
 
+/*filtros por categoria y nombre del producto*/
+displayProducts();
 
-
-displayProducts()
-
-titleFilterEl.addEventListener('input', () => {
-    console.log('title-filter changed:', titleFilterEl.value);
-    displayProducts(titleFilterEl.value, categoryFilterEl.value);
+titleFilterEl.addEventListener("input", () => {
+  console.log("title-filter changed:", titleFilterEl.value);
+  displayProducts(titleFilterEl.value, categoryFilterEl.value);
 });
 
-categoryFilterEl.addEventListener('change', () => {
-    console.log('category-filter changed:', categoryFilterEl.value);
-    displayProducts(titleFilterEl.value, categoryFilterEl.value);
+categoryFilterEl.addEventListener("change", () => {
+  console.log("category-filter changed:", categoryFilterEl.value);
+  displayProducts(titleFilterEl.value, categoryFilterEl.value);
 });
-
